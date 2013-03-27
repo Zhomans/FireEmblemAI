c53045a5c79e09bdc8f114b07875eb39445c5151
esm8m <elizabeth.mahon@students.olin.edu>
2013-03-27 17:00:31 -0400

    Basic map creation and unit framework.

---
+++ unit.py
@@ -0,0 +1,19 @@
+#Represents a single unit
+#Jazmin Gonzalez-Rivero, Zachary Homans, Elizabeth Mahon, Brendan Ritter
+#Artificial Intelligence, Olin College, Spring 13
+
+class unit:
+    #units have:
+    #   the space they're on
+    #   their class
+    #   their weapons
+    #   their stats
+    #all we need at the beginning is the space.
+    #everything else will be constant.
+    def __init__(self, space = (1,1)):
+        self.space = space
+        self.attack = 10
+    def add_space(space):
+        #units need to know what space they're on
+        #need some way to make sure that units and spaces are always paired
+        self.space = space
