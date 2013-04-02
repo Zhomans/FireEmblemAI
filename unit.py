2ea4a6fba4442a20fe2eb68643e3153c6dba9262
esm8m <elizabeth.mahon@students.olin.edu>
2013-04-02 11:17:40 -0400

    Tests changed to work. Units can now move. Need to figure out what to do if given an invalid move.

--- unit.py
+++ unit.py
@@ -10,11 +10,19 @@ class unit:
     #   their stats
     #all we need at the beginning is the space.
     #everything else will be constant.
-    def __init__(self, space = (1,1)):
+    def __init__(self, space = None):
         self.space = space
         self.attack = 10
         self.hp = 35
+        self.move = 5 #units can move 5 spaces by default
     def add_space(self, space):
         #units need to know what space they're on
         #need some way to make sure that units and spaces are always paired
         self.space = space
+    def move(self, space):
+        deltax = abs(self.space.get_coords()[1] - space.get_coords()[1])
+        deltay = abs(self.space.get_coords()[2] - space.get_coords()[2])
+        if (deltax + deltay) <= self.space:
+            self.space = space
+        else:
+            #error!!
