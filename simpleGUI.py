ae540a3dbcabb9d8dc5b3994820e811ae4b09e33
brendan <brendan.ritter@yahoo.com>
2013-04-02 11:02:07 -0400

    Trying to add images to the GUI

--- simpleGUI.py
+++ simpleGUI.py
@@ -2,6 +2,10 @@ import Tkinter as tk
 board=[[None]*10 for _ in range(10)]
 counter=0
 root=tk.Tk()
+photo=tk.PhotoImage(file="dude.gif")
+label=tk.Label(root,image=photo)
+label.image=photo
+label.pack()
 for i,row in enumerate(board):
 	for j,column in enumerate(row):
 		if ((i+j) % 2) ==0:
