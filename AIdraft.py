7f51af5986398e14ccac4a3ee0da4859db83dcea
esm8m <elizabeth.mahon@students.olin.edu>
2013-04-09 11:19:22 -0400

    Wrote a note about how we'll want to implement it.

--- AIdraft.py
+++ AIdraft.py
@@ -4,13 +4,12 @@ consider = list()
 for unit in units:
     #assuming there's also a list of enemy units
     for enemy in enemies:
+        #check if enemy is in range
         xdistance = abs(unit.get_space().get_coords()[1] - enemy.get_space().get_coords[1]) #this is terrible. Need to redo how you get this stuff
         ydistance = abs(unit.get_space().get_coords()[2] - enemy.get_space().get_coords[2])
         if (xdistance + ydistance) <= unit.move:
             #if it's in range, consider it
             consider.append(enemy)
-    weakest = consider[1]
-    for enemy in consider:
-        if enemy.HP < weakest.HP:
-            weakest = enemy
-        
+    #dictionary that maps from enemies to damage we can do to it
+    #update with each unit
+    #at the end, we'll go for the one with the most, right?
