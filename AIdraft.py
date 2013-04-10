a91d2711bf1cf85acc4a3c1133536c8d88386771
esm8m <elizabeth.mahon@students.olin.edu>
2013-04-10 16:30:06 -0400

    Started to figure out how to limit # of attackers

--- AIdraft.py
+++ AIdraft.py
@@ -1,42 +1,52 @@
+#This is DEFINITELY not optimal, either in terms of speed or intelligence.
+#But premature optimization is a bad idea!
+#So the goal is to get it working!
+
 #units is a list of usable units the AI owns
 #actedUnits is a list of unusable units the AI owns
 
-tot_damage = dict()
+damage = dict()
 attackers = dict()
 
 
 while length(units) != length(actedUnits):
 #while there are units that are usable
     for unit in units:
-    #assuming there's also a list of enemy units
-        for enemy in enemies:
+        for enemy in self.opponent.units:
             #check if enemy is in range
             xdistance = abs(unit.get_space().get_x() - enemy.get_space().get_x())
             ydistance = abs(unit.get_space().get_y() - enemy.get_space().get_y())
             if (xdistance + ydistance) <= unit.move:
-                #if it's in range, note the damage you can do
-                tot_damage[enemy] = tot_damage[enemy] + unit.attack
-                #and remember that you can attack it
-                if enemy in attackers:
-                    attackers[enemy] = attackers[enemy].append(unit)
+                if enemy in attackers.keys():
+                    #if other units can attack the enemy, check if you should
+                    if(length(attackers[enemy]) < 4):
+                        attackers[enemy] = attackers[enemy].append(unit)
+                        damage[enemy] = damage[enemy].append(unit)
+                    else:
+                        #already has maximum number of units attacking it
+                        #check whether it's better for this unit to attack
+
+                        #assume order of damage list is same as
+                        #order of attacker list
+                        #which should be true
+                        low_dmg = min(damage[enemy])
+                        if((unit.attack - enemy.defense) > low_dmg):
+                            #stronger than a current attacker, replace it
+                            index = damage[enemy].index(low_dmg)
+                            damage[enemy][index] = unit.attack - enemy.defense
+                            attackers[enemy][index] = unit
                 else:
+                    #if no one else can attack it, remember that you can
                     attackers[enemy] = list(unit)
-    #loop through the dictionary of enemies that can be attacked?
-    #there's gotta be a faster way.
-    #Premature optimization is the death of working code
-    #so let's just do the inefficient way
-    #after we move all units for first enemy, do we want to recalculate?
-    #so if we do so
-    #we don't actually need a list
-    #we only need the weakest one.
+                    damage[enemy] = list(unit.attack - enemy.defense)
 
     #arbitrarily initialize weakest as the last one we looked at
     weakest = enemy
 
-    for enemy in tot_damage.keys():
-        if(tot_damage[enemy]/enemy.hp < tot_damage[weakest]/weakest.hp):
+    for enemy in damage.keys():
+        if(sum(damage[enemy])/enemy.hp < sum(damage[weakest])/weakest.hp):
             weakest = enemy
-        elif (tot_damage[enemy]/enemy.hp == tot_damage[weakest]/weakest.hp):
+        elif (sum(damage[enemy])/enemy.hp == sum(damage[weakest])/weakest.hp):
             #attack the one with less health if % same
             if weakest.hp > enemy.hp:
                 weakest = enemy
