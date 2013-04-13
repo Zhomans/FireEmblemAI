592e4487b36c0f387591e0f8f180e283c9d5b961
esm8m <elizabeth.mahon@students.olin.edu>
2013-04-12 23:28:58 -0400

    Added comments to make sure I understand it. Attempted to make it so the best unit attacks first.

--- AI_ZachAttempt.py
+++ AI_ZachAttempt.py
@@ -12,20 +12,26 @@ for enemy in opponent.units:
 available_units = units
 while available_units != []:
     for unit in available_units:
+        #unit is of type unit
         unit.move_list = unit.get_move_list()
         unit.attack_list = unit.get_attack_list()
 
         for enemy in opponent.units:
+            #enemy is of type unit
             if enemy.space in unit.attack_list:
                 surrounding_spaces = []
                 for move_poss in ([0, 1], [0, -1], [1, 0], [-1, 0]):
                     surrounding_space=world.get_space(enemy.space.get_x()+move_poss[0], enemy.space.get_y()+move_poss[1])
                     if surrounding_space in unit.move_list:
+                        #check which spaces the unit can attack the enemy from
                         surrounding_spaces.append(surrounding_space)
 
+            #for every enemy, remember which units can attack it
+            #how much they can do, and where they can attack from
             enemies[enemy] = enemies[enemy].append(list(unit, unit.damage, surrounding_spaces))
 
     for enemy in opponent.units:
+        #enemy is of type unit
         top_space = world.get_space(enemy.space.get_x(), enemy.space.get_y()-1)
         units_that_can_attack_top = []
         bottom_space = world.get_space(enemy.space.get_x(), enemy.space.get_y()+1)
@@ -36,6 +42,8 @@ while available_units != []:
         units_that_can_attack_right = []
 
         for unit in enemies[enemy]:
+            #unit is a list of a unit, an int (damage), and a list of spaces
+            #get lists of who can attack from each space
             if top_space in unit[2]:
                 units_that_can_attack_top.append(unit)
             if bottom_space in unit[2]:
@@ -52,7 +60,10 @@ while available_units != []:
         left_attacker = None
 
 
+        #these next four for loops determine which unit can do the most damage
+        #in the position that it's labelled for
         for unit in units_that_can_attack_top:
+            #unit is a list of a unit, an int (damage), and a list of spaces
             if top_attacker == None:
                 top_attacker = unit[0]
             else:
@@ -89,6 +100,9 @@ while available_units != []:
 
         
         while final_attackers == false:
+            #check whether the same unit is in two slots
+            #if so, find the next best one for one of the slots
+            #only loops if duplicates are found
             final_attackers = true
 
             if top_attacker == bottom_attacker and top_attacker != None:
@@ -96,6 +110,7 @@ while available_units != []:
 
                 new_top_attacker = None
                 for unit in units_that_can_attack_top:
+                    #unit is a list of a unit, an int (damage), and a list of spaces
                     if new_top_attacker == None:
                         new_top_attacker = unit[0]
                     else:
@@ -273,6 +288,7 @@ while available_units != []:
     max_damage_percent = 0
     optimal_target = None
     for enemy in opponent.units:
+        #figure out which enemy to attack first
         if optimal_target = None:
             optimal_target = enemy
         else:
@@ -280,12 +296,33 @@ while available_units != []:
                 optimal_target = enemy
 
     #Change so only the strongest attacks before recalculation.
-    enemy_attack[optimal_target][0].move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()-1))
-    enemy_attack[optimal_target][0].attack(optimal_target)
-    enemy_attack[optimal_target][1].move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()+1))
-    enemy_attack[optimal_target][1].attack(optimal_target)
-    enemy_attack[optimal_target][2].move_unit(world.get_space(enemy.space.get_x()-1, enemy.space.get_y()))
-    enemy_attack[optimal_target][2].attack(optimal_target)
-    enemy_attack[optimal_target][3].move_unit(world.get_space(enemy.space.get_x()+1, enemy.space.get_y()))
-    enemy_attack[optimal_target][3].attack(optimal_target)
+    #4-12: I think I got it, so I commented out the first draft. My attempt is below.
+    #Let me know if it looks right, Zach. -Elizabeth
+
+    #enemy_attack[optimal_target][0].move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()-1))
+    #enemy_attack[optimal_target][0].attack(optimal_target)
+    #enemy_attack[optimal_target][1].move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()+1))
+    #enemy_attack[optimal_target][1].attack(optimal_target)
+    #enemy_attack[optimal_target][2].move_unit(world.get_space(enemy.space.get_x()-1, enemy.space.get_y()))
+    #enemy_attack[optimal_target][2].attack(optimal_target)
+    #enemy_attack[optimal_target][3].move_unit(world.get_space(enemy.space.get_x()+1, enemy.space.get_y()))
+    #enemy_attack[optimal_target][3].attack(optimal_target)
+
+    #arbitrarily initialize best attacker as first one
+    move_next = enemy_attack[optimal_target][0]
+    for attacker in enemy_attack[optimal_target]:
+        if attacker.damage > move_next.damage:
+            move_next = attacker
+
+    #figure out which slot the next unit to move was in then move it there
+    slot = enemy_attack[optimal_target].index(move_next)
+    if slot == 0:
+        move_next.move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()-1))
+    elif slot == 1:
+        move_next.move_unit(world.get_space(enemy.space.get_x(), enemy.space.get_y()+1))
+    elif slot == 2:
+        move_next.move_unit(world.get_space(enemy.space.get_x()-1, enemy.space.get_y()))
+    elif slot == 3:
+        move_next.move_unit(world.get_space(enemy.space.get_x()+1, enemy.space.get_y()))
+    move_next.attack(optimal_target)
     
