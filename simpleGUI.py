9c4826d5ba6578cf4ab696c5666fa1831db74121
Jazmin <jazmin.gonzalez-rivero@students.olin.edu>
2013-04-15 13:48:33 -0400

    Add image of the Unit works if the right sized image is used

--- simpleGUI.py
+++ simpleGUI.py
@@ -1,12 +1,21 @@
 import Tkinter as tk
-board=[[None]*10 for _ in range(10)]
-counter=0
-root=tk.Tk()
-for i,row in enumerate(board):
-	for j,column in enumerate(row):
-		if ((i+j) % 2) ==0:
-			L=tk.Label(root,text='    ',bg='green')
-		else:
-			L=tk.Label(root,text='    ',bg='dark green')
-		L.grid(row=i,column=j)
-root.mainloop()
+import feworld
+from PIL import ImageTk
+from PIL import Image
+
+def display(maps):
+	board=[[None]*10 for _ in range(10)]
+	counter=0
+	root=tk.Tk()
+	for i,row in enumerate(board):
+		for j,column in enumerate(row):
+			if (maps.grid[i][j].unit == None):
+				if ((i+j) % 2) ==0:
+					L=tk.Label(root,text='    ',bg='green')
+				else:
+					L=tk.Label(root,text='    ',bg='dark green')
+			else:
+				img = ImageTk.PhotoImage(Image.open('dude.gif'))
+				L = tk.Label(root, image = img)
+			L.grid(row=i,column=j)
+	root.mainloop()
