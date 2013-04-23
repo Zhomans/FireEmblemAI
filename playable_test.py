847638751971cccadf6f546291d773b5b0301d00
brendan <brendan.ritter@yahoo.com>
2013-04-23 16:16:41 -0400

    Made the GUI not suck. More changes coming,

--- playable_test.py
+++ playable_test.py
@@ -2,10 +2,31 @@ from Player import *
 from unit import *
 from feworld import *
 from AI_ZachAttempt import *
-from simpleGUI import *
+import Tkinter as tk
+from PIL import ImageTk
+from PIL import Image
+
+def display(maps,root,good,bad):
+    for i,row in enumerate(maps.grid):
+        for j,column in enumerate(row):
+            if (maps.grid[i][j].unit == None):
+                if ((i+j) % 2) ==0:
+                    L=tk.Label(root,text='    ',bg='green')
+                else:
+                    L=tk.Label(root,text='    ',bg='dark green')
+            else:
+                if maps.grid[i][j].unit.player.name == 'human':
+                    L = tk.Label(root, image = good)
+                else:
+                    L = tk.Label(root, image = bad)
+            L.grid(row=j,column=i)
+
 
 #initialize everything
 level = fe_map()
+root=tk.Tk()
+player_img = ImageTk.PhotoImage(Image.open('dude.gif'))
+enemy_img = ImageTk.PhotoImage(Image.open('bandit.gif'))
 
 #player units
 #Looks like in 7 you get ~.03 units/space
@@ -32,14 +53,14 @@ com.initialize(human,list(enemy_units),"com", "com")
 
 while(len(human.units) == len(player_units) and len(com.units) == len(enemy_units)):
     #run while no units have died
-    display(level)
+    display(level,root,player_img,enemy_img)
     while(len(human.movedUnits) != len(human.units)):
         human.play_turn(level)
-        display(level)
+        display(level,root,player_img,enemy_img)
 
     while (len(com.units) != len(com.actedUnits)):
         com.play_turn(level)
-        display(level)
+        display(level,root,player_img,enemy_img)
     #reset everything
     human.movedUnits = []
     human.actedUnits = []
@@ -49,3 +70,4 @@ if len(human.units) != len(player_units):
     print "Computer wins!"
 else:
     print "Human wins!"
+root.mainloop() 
