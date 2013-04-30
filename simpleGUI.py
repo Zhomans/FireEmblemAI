f07647b12d06030cbaf7b1d6316331d0b61c12ed
Zach Homans <zachary.homans@students.olin.edu>
2013-04-29 20:41:03 -0400

    Fixed Forest

--- simpleGUI.py
+++ simpleGUI.py
@@ -12,27 +12,25 @@ def display(maps, root):
 	dirt_img = ImageTk.PhotoImage(Image.open('dirt.gif'))
 	for i in range(maps.x):
 		for j in range(maps.y):
-			if (maps.grid[i][j].unit == None):
-				if maps.grid[i][j].terrain.terrainType == 'forest':
-					L = tk.Label(root, image = forest_img)
-					L.image = forest_img
-				if maps.grid[i][j].terrain.terrainType == 'desert':
-					L = tk.Label(root, image = desert_img)
-					L.image = desert_img
-				if maps.grid[i][j].terrain.terrainType == 'mountain':
-					L = tk.Label(root, image = mountain_img)
-					L.image = mountain_img
-				if maps.grid[i][j].terrain.terrainType == 'dirt':
-					L = tk.Label(root, image = dirt_img)
-					L.image = dirt_img
-				else:
-					L=tk.Label(root,text='    ',bg='green')
+			if maps.grid[i][j].terrain.terrainType == 'desert':
+				L = tk.Label(root, image = desert_img)
+				L.image = desert_img
+			if maps.grid[i][j].terrain.terrainType == 'mountain':
+				L = tk.Label(root, image = mountain_img)
+				L.image = mountain_img
+			if maps.grid[i][j].terrain.type == 'dirt':
+				L = tk.Label(root, image = dirt_img)
+				L.image = dirt_img
 			else:
+				L = tk.Label(root, image = forest_img)
+				L.image = forest_img
+			L.grid(row=j,column=i)
+
+			if (maps.grid[i][j].unit != None):
 				if maps.grid[i][j].unit.player.name == 'human':
 					L = tk.Label(root, image = player_img)
 					L.image = player_img
 				else:
 					L = tk.Label(root, image = enemy_img)
 					L.image = enemy_img
-			L.grid(row=j,column=i)
-
+				L.grid(row=j,column=i)
