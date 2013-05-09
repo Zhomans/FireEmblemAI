484e240cf5f799141f27cc0d126b72dc3bcafe8b
Jazmin <jazmin.gonzalez-rivero@students.olin.edu>
2013-05-09 16:05:34 -0400

    made it so 2 comps could play against eachother

--- comVcomTest.py
+++ comVcomTest.py
@@ -22,28 +22,24 @@ for j in range(1,12):
 for i in range(50):
     level.grid[int(random.randint(0,19))][int(random.randint(0,19))].terrain = terrain('forest')
 
-#player units
-#Looks like in 7 you get ~.03 units/space
-eliwood = unit(world = level, space = level.get_space(0,0), name = "eliwood")
-hector = unit(world = level, space = level.get_space(0,1), name = "hector", hp = 30)
-lyn = unit(world = level, space = level.get_space(2,0), name = "lyn", hp = 15)
-roy = unit(world = level, space = level.get_space(2,2), name = "roy", hp = 15)
-erika = unit(world = level, space = level.get_space(1,1), name = "erika", hp = 18)
-ephraim = unit(world = level, space = level.get_space(2,3), name = "ephraim", hp = 25)
-player_units = [eliwood, hector, lyn, roy, erika, ephraim]
-
 #enemy units
 #in 7 there's just under twice as many enemies as your units at first
 #then a bunch of reinforcements
 #we aren't accounting for reinforcements....
+enemy2_units = []
+for i in range (8):
+    hero = unit(world=level,space=level.get_space(i, 19-i),name="hero"+str(i))
+    enemy2_units.append(hero)
+
 enemy_units = []
 for i in range(8):
     grunt = unit(world=level,space=level.get_space(12+i, 19-i),name="grunt"+str(i))
     enemy_units.append(grunt)
 
 com = player()
-#note: python lists are passed by reference, so the copy is necessary
-human = player(com, list(player_units), "human", "human")
-com.initialize(human,list(enemy_units),"com", "com")
+com2 = player()
 
-play_game(root, level, human, com, strat2 = "t")
+com.initialize(com2,list(enemy_units),"com", "com")
+com2.initialize(com, list(enemy2_units), "com2", "com")
+
+play_game(root, level, com2, com, "t", "a")
